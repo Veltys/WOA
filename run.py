c939e15af4e8e57f06fab4f870c1b026f312f129
Veltys <veltys@veltys.es>
2021-04-25 06:34:42 +0200

    ADD: Functions exported to a separated file

--- run.py
+++ run.py
@@ -5,9 +5,8 @@
 import argparse
 import sys
 
-import numpy as np
-
 from src.animate_scatter import AnimateScatter
+import src.funcs
 from src.whale_optimization import WhaleOptimization
 
 
@@ -27,64 +26,13 @@ def parseClArgs():
     return args
 
 
-# optimization functions from https://en.wikipedia.org/wiki/Test_functions_for_optimization
-
-
-def schaffer(x, y):
-    """constraints=100, minimum f(0,0)=0"""
-    numer = np.square(np.sin(x ** 2 - y ** 2)) - 0.5
-    denom = np.square(1.0 + (0.001 * (x ** 2 + y ** 2)))
-
-    return 0.5 + (numer * (1.0 / denom))
-
-
-def eggholder(x, y):
-    """constraints=512, minimum f(512, 414.2319)=-959.6407"""
-    y = y + 47.0
-    a = (-1.0) * (y) * np.sin(np.sqrt(np.absolute((x / 2.0) + y)))
-    b = (-1.0) * x * np.sin(np.sqrt(np.absolute(x - y)))
-    return a + b
-
-
-def booth(x, y):
-    """constraints=10, minimum f(1, 3)=0"""
-    return ((x) + (2.0 * y) - 7.0) ** 2 + ((2.0 * x) + (y) - 5.0) ** 2
-
-
-def matyas(x, y):
-    """constraints=10, minimum f(0, 0)=0"""
-    return (0.26 * (x ** 2 + y ** 2)) - (0.48 * x * y)
-
-
-def crossInTray(x, y):
-    """constraints=10,
-    minimum f(1.34941, -1.34941)=-2.06261
-    minimum f(1.34941, 1.34941)=-2.06261
-    minimum f(-1.34941, 1.34941)=-2.06261
-    minimum f(-1.34941, -1.34941)=-2.06261
-    """
-    B = np.exp(np.absolute(100.0 - (np.sqrt(x ** 2 + y ** 2) / np.pi)))
-    A = np.absolute(np.sin(x) * np.sin(y) * B) + 1
-    return -0.0001 * (A ** 0.1)
-
-
-def levi(x, y):
-    """constraints=10,
-    minimum f(1,1)=0.0
-    """
-    a = np.sin(3.0 * np.pi * x) ** 2
-    b = ((x - 1) ** 2) * (1 + np.sin(3.0 * np.pi * y) ** 2)
-    c = ((y - 1) ** 2) * (1 + np.sin(2.0 * np.pi * y) ** 2)
-    return a + b + c
-
-
 def main(argv): # @UnusedVariable
     args = parseClArgs()
 
     nSols = args.nsols
     nGens = args.ngens
 
-    funcs = { 'schaffer': schaffer, 'eggholder': eggholder, 'booth': booth, 'matyas': matyas, 'cross': crossInTray, 'levi': levi }
+    funcs = { 'schaffer': src.funcs.schaffer, 'eggholder': src.funcs.eggholder, 'booth': src.funcs.booth, 'matyas': src.funcs.matyas, 'cross': src.funcs.crossInTray, 'levi': src.funcs.levi }
     funcConstraints = { 'schaffer': 100.0, 'eggholder': 512.0, 'booth': 10.0, 'matyas': 10.0, 'cross': 10.0, 'levi': 10.0 }
 
     if args.func in funcs:
